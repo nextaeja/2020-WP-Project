8657ca292f0b5e33f2a20ef9d693808b2fd49eff
Nextaeja <nextaeja@gmail.com>
2020-09-03 00:04:12 -0700

    Compile Cuda While
    Works now

--- Beta4_2/mexcudawhile.cu
+++ Beta4_2/mexcudawhile.cu
@@ -3,9 +3,9 @@
 #include <math.h>
 #include <cufft.h>
 
-#include "../MEX_helpers/complex.h"
-#include "../MEX_helpers/cuda_helper.h"
-#include "../Setup/cuda_setup_dynamic_potential.h"
+#include "MEX_helpers/complex.h"
+#include "MEX_helpers/cuda_helper.h"
+#include "Setup/cuda_setup_dynamic_potential.h"
 
 const int NUM_GAUSSIAN_ADSORBATE_DIMENSIONS = 2;
 
